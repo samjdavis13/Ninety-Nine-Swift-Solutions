befa34b571f26bb10d62740576d0ae2d270b32d7
Sam Davis <sam@sjd.co>
2016-05-24 00:09:54 +1000

    add solution 2 (alt)

---
+++ p02-alt_samjdavis13.swift
@@ -0,0 +1,14 @@
+//
+//  p04_samjdavis13.swift
+//  
+//
+//  Created by Sam Davis on 24/05/2016.
+//
+//
+
+// Utilises my solution to problem 4 in order to function
+extension List {
+    var pennultimateAlt: T? {
+        return self[self.length - 2]
+    }
+}
